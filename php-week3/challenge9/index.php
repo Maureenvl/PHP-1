e2bca3eba16854f018d07508e8e07e013b6c3af3
Maureen <220000@student.glu.nl>
2023-01-27 15:08:43 +0100

    opdracht 9

---
+++ php-week3/challenge9/index.php
@@ -0,0 +1,44 @@
+<?php
+// echo readfile("img/files.txt");
+
+$somecontent = "";
+$dir = "img";
+
+foreach (glob( "img/.jpg") as $filename){
+    //displays all pngs and their name
+    echo "<img src='$filename' alt='$filename' height='100px' /><br>";
+    echo "<p>$filename</p>";
+}
+
+foreach (glob( "img/.jpeg") as $filename){
+    //displays all pngs and their name
+    echo "<img src='$filename' alt='$filename' height='100px' /><br>";
+    echo "<p>$filename</p>";
+}
+
+foreach (glob( "img/*.png") as $filename){
+    //displays all pngs and their name
+    echo "<img src='$filename' alt='$filename' height='100px' /><br>";
+    echo "<p>$filename</p>";
+}
+
+if (is_dir($dir)) {
+    if ($dh = opendir($dir)) {
+        while (($file = readdir($dh)) !== false) {
+            if ($file == "." || $file == "..") {
+                continue;
+            } 
+
+            echo $file . " grootte: ";
+            $somecontent .= $file . " ";
+            echo filesize($dir . "/" . $file) . "<br>";
+            $somecontent .= filesize($dir . "/" . $file) . "\n";
+        }
+        closedir($dh);
+    }
+}
+
+$txtfile = fopen("img/files.txt", "w");
+echo fwrite($txtfile, $somecontent);
+fclose($txtfile);
+?>
